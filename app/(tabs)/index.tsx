36f4ec6ca922fb38f774d81bfe7e646e3d427c19
royarispe <royarispe001@gmail.com>
2024-10-30 23:00:02 -0300

    ta3-full

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,109 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from 'react';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+type movie = {
+  Poster: string,
+  Title: string,
+  Plot: string
+}
 
 export default function HomeScreen() {
+
+  const [movieName, setMovieName] = useState<string>("")
+  const [movie, setMovie] = useState<movie | null>(null)
+
+
+  const getMovie = async () => {
+    const response = await fetch(`http://www.omdbapi.com/?t=${movieName}&apikey=401020e8`)
+    const data = await response.json();
+    if (response.ok) {
+      setMovie(data)
+    }
+  }
+
+  const handleSearch = () => {
+    getMovie()
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text style={styles.textMain}>BUSCADOR DE PELÍCULAS</Text>
+          <TextInput onChangeText={setMovieName} style={styles.input} placeholder='ingrese la película a buscar'></TextInput>
+          <TouchableOpacity onPress={handleSearch} style={styles.btnSearch}>
+            <Text style={styles.btnText}>Buscar</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.containerMovie}>
+          <Image source={{ uri: movie?.Poster }} style={styles.image}></Image>
+          <Text style={styles.title}>{movie?.Title}</Text>
+          <Text style={styles.plot}>{movie?.Plot}</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
+    backgroundColor: '#f0f0f0',
+    paddingTop: 40,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    width: '100%',
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: '#fff',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  containerMovie: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 'auto',
+    width: '80%',
+    borderRadius: 50,
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: '#52b788'
+  },
+  btnSearch: {
+    borderWidth: 1,
+    backgroundColor: 'blue',
+    width: '25%',
+  },
+  btnText: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    color: 'white',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderWidth: 3,
+    borderColor: '#d8f3dc'
+  },
+  title: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  plot: {
+    borderWidth: 1,
+    margin: 10,
+    padding: 5,
+    backgroundColor: '#d8f3dc'
+  },
+  textMain: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
 });
